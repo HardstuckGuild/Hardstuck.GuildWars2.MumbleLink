1e60f5e5000d2dec61b1b769ecb6ab727d46467d
Plenyx <1507236+Plenyx@users.noreply.github.com>
2023-02-01 10:49:21 +0100

    Switch to System.Text.Json

--- src/Structs/LinkedMem.cs
+++ src/Structs/LinkedMem.cs
@@ -1,5 +1,5 @@
 ﻿using System.Runtime.InteropServices;
-using Newtonsoft.Json;
+using System.Text.Json;
 
 namespace Hardstuck.GuildWars2.MumbleLink
 {
@@ -74,7 +74,20 @@ namespace Hardstuck.GuildWars2.MumbleLink
         /// <summary>
         /// The character's identity
         /// </summary>
-        public Identity Identity => JsonConvert.DeserializeObject<Identity>(_IdentityString);
+        public Identity Identity
+        {
+            get
+            {
+                try
+                {
+                    return JsonSerializer.Deserialize<Identity>(_IdentityString);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
 
         /// <summary>
         /// Character length of the context
